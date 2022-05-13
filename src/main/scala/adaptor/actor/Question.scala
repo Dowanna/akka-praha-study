629b817bbcc141c59959b35c660925573c5c46d5
Kyosuke Awata <kyou0209@gmail.com>
2022-05-13 12:58:32 +0900

    wip

--- src/main/scala/adaptor/actor/Question.scala
+++ src/main/scala/adaptor/actor/Question.scala
@@ -1,13 +1,25 @@
 package adaptor.actor
 
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
+import domain.Answer
 
 object QuestionActor {
   sealed trait Command
+  case class AddAnswer(answer: Answer) extends Command
+
   def apply(): Behavior[Command] = {
-    Behaviors.receive { (ctx, msg) =>
-      Behaviors.same
+    create(Vector.empty);
+  }
+  
+  private def create(answerActors: Vector[ActorRef[]]) :Behavior[Command] = {
+    Behaviors.receive{ (ctx, msg) => 
+      msg match {
+        case AddAnswer =>
+          Behaviors.empty
+          // answerActorをspawn
+          // spawnしたactorRef+元々のやつを足して、引数として再度createを呼ぶ
+      }
     }
   }
 }
