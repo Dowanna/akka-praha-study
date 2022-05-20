f54e9216cf3d9996823ba0bc628200edaf8d019a
Kyosuke Awata <kyou0209@gmail.com>
2022-05-20 13:05:24 +0900

    wip

--- src/main/scala/adaptor/actor/Question.scala
+++ src/main/scala/adaptor/actor/Question.scala
@@ -1,18 +1,20 @@
 package adaptor.actor
 
 import adaptor.actor.AnswerActor.GetAnswerResponse
-import akka.actor.Status.Success
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.util.Timeout
 import domain.Answer
 
 import scala.concurrent.duration.DurationInt
+import scala.util.{Failure, Success, Try}
 
 object QuestionActor {
   sealed trait Command
   case class AddAnswer(answer: Answer) extends Command
   case class GetAllAnswers() extends Command
+
+  private case class AdaptedResponse(getAnswerResponse: GetAnswerResponse) extends Command
 
   def apply(): Behavior[Command] = {
     create(Vector.empty);
@@ -26,10 +28,14 @@ object QuestionActor {
           create(answerActors :+ newAnswerActor)
         case GetAllAnswers() =>
           implicit val timeout: Timeout = 3.seconds
-          answerActors.map(answerActor => ctx.ask(answerActor, AnswerActor.GetAnswer.apply)({
-//            case Success(value) => "hoge"
-//            case Failure(exception) => "hoge"
-          }))
+
+          answerActors.map(answerActor => ctx.ask(answerActor, AnswerActor.GetAnswer.apply) {
+            case Success(v1) =>
+              AdaptedResponse(v1)
+            case Failure(exception) =>
+              AdaptedResponse(null)
+          })
+
           Behaviors.same
       }
     }
