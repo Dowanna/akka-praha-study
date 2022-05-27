2aa5e26101079f94ff02d317d96efe4a27265977
shunya matsubara <shun.fzs1000@gmail.com>
2022-05-27 12:16:42 +0900

    wip

--- src/main/scala/adaptor/actor/Question.scala
+++ src/main/scala/adaptor/actor/Question.scala
@@ -12,7 +12,8 @@ import scala.util.{Failure, Success, Try}
 object QuestionActor {
   sealed trait Command
   case class AddAnswer(answer: Answer) extends Command
-  case class GetAllAnswers() extends Command
+  case class GetAllAnswers(replyTo: ActorRef[GetAllAnswersResponse]) extends Command
+  case class GetAllAnswersResponse(answers: Vector[Answer]) extends Command
 
   private case class AdaptedResponse(getAnswerResponse: GetAnswerResponse) extends Command
 
@@ -20,23 +21,31 @@ object QuestionActor {
     create(Vector.empty);
   }
 
-  private def create(answerActors: Vector[ActorRef[AnswerActor.Command]]): Behavior[Command] = {
+  private def create(answerActors: Vector[ActorRef[AnswerActor.Command]], answers: Vector[Answer]): Behavior[Command] = {
     Behaviors.receive { (ctx, msg) =>
       msg match {
         case AddAnswer(answer) =>
           val newAnswerActor = ctx.spawn(AnswerActor(answer), s"answer-${answer.id}")
           create(answerActors :+ newAnswerActor)
-        case GetAllAnswers() =>
+        case GetAllAnswers(replyTo) =>
           implicit val timeout: Timeout = 3.seconds
-
-          answerActors.map(answerActor => ctx.ask(answerActor, AnswerActor.GetAnswer.apply) {
-            case Success(v1) =>
-              AdaptedResponse(v1)
-            case Failure(exception) =>
-              AdaptedResponse(null)
-          })
-
+          answerActors.map(answerActor =>
+            ctx.ask(answerActor, AnswerActor.GetAnswer.apply) {
+              case Success(v1) =>
+                AdaptedResponse(v1)
+              case Failure(exception) =>
+                AdaptedResponse(null)
+            }
+          )
           Behaviors.same
+        case AdaptedResponse(getAnswerResponse) => {
+          getAnswerResponse match {
+            case null =>
+              Behaviors.same
+            case answer =>
+              create(answerActors, answers :+ answer.answer)
+          }
+        }
       }
     }
   }
