75bd3bbc1c9d88923ef2382893d9bce6de9eaaa8
shunya matsubara <shun.fzs1000@gmail.com>
2022-07-22 12:48:37 +0900

    wip

--- src/main/scala/adaptor/actor/PersistentQuestionActor.scala
+++ src/main/scala/adaptor/actor/PersistentQuestionActor.scala
@@ -3,19 +3,22 @@ package adaptor.actor
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
 import domain.Answer
+import domain.Question
 
 object PersistentQuestionActor {
   sealed trait Command
   final case class AddAnswer(answer: Answer) extends Command
 
   sealed trait Event
+  final case class AddedAnswerToQuestion(question: Question) extends Event
+  case object AddAnswerFailed extends Event
 
-  final case class State()
+  final case class State(question: Question)
 
-  def Behavior = {
+  def Behavior(id: String, question: Question) = {
     EventSourcedBehavior[Command, Event, State](
-      persistenceId = PersistenceId.ofUniqueId("question"),
-      emptyState = State(),
+      persistenceId = PersistenceId("question", id), // 他のpersistent-actorと被ったらどうなるんだろう?
+      emptyState = State(question),
       commandHandler = (state, command) => throw new NotImplementedError("TODO: process the command & return an Effect"),
       eventHandler = (state, event) => throw new NotImplementedError("TODO: process the event return the next state")
     )
@@ -23,6 +26,17 @@ object PersistentQuestionActor {
 
   val commandHandler: (State, Command) => Effect[Event, State] = (state, command) =>
     command match {
-
+      case AddAnswer(answer) => {
+        Question(id = state.question.id, title = state.question.title, body = state.question.body, answers = state.question.answers + answer, tags = state.question.tags) match {
+          case Left(value) => Effect.persist(AddAnswerFailed)
+          case Right(value) =>
+            Effect.persist(AddedAnswerToQuestion(value))
+        }
+      }
+    }
+  val eventHandler: (State, Event) => State = (state, event) =>
+    event match {
+      case AddedAnswerToQuestion(question) => State(question)
+      case AddAnswerFailed => state
     }
 }
